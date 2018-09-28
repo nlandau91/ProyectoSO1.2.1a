78ebae817bd8e8e360f8fdec4b73718d82b4f8c1
nlandau91 <43214457+nlandau91@users.noreply.github.com>
2018-09-28 14:33:39 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -5,48 +5,51 @@
 #include <unistd.h>
 #define MAXUSERS 30
 
-pthread_t threads[MAXUSERS];
 typedef struct{
     sem_t sem1;
     sem_t sem2;
 }   semaforos;
 
-int requerir(void *sem){
+int requerir(semaforos *sem){//espero a la primer impresora que se desocupe.
     while(1){
-        if(sem_trywait(&((semaforos *)sem)->sem1) == 0){
+        if(sem_trywait(&sem->sem1) == 0){
             return 1;
         }
-        if(sem_trywait(&((semaforos *)sem)->sem2) == 0){
+        if(sem_trywait(&sem->sem2) == 0){
             return 2;
         }
     }
 }
 
-void liberar(int impresora,void *sem){
+void liberar(int impresora,semaforos *sem){//desbloqueo la impresora
     if(impresora==1){
-        sem_post(&((semaforos *)sem)->sem1);
+        sem_post(&sem->sem1);
     }
     if(impresora==2){
-        sem_post(&((semaforos *)sem)->sem2);
+        sem_post(&sem->sem2);
     }
 }
 
-void imprimir(int impresora){
+void imprimir(int impresora){//imprimo
     printf("estoy imprimiento en la impresora %d.\n",impresora);
+    sleep(1);
+    printf("termine de imprimir en la impresora %d.\n",impresora);
+
 }
 
-void *hilo(void *sem){
-    int n =requerir(sem);
-    imprimir(n);
-    liberar(n,sem);
+void *hilo(semaforos *sem){
+    int n =requerir(sem);//bloqueo
+    imprimir(n);//seccion critica
+    liberar(n,sem);//desbloqueo
     pthread_exit(EXIT_SUCCESS);
 }
 
 int main()
 {
+    pthread_t threads[MAXUSERS];
     semaforos *sem = (semaforos *)malloc(sizeof(semaforos));
-    sem_init(&sem->sem1,0,1);
-    sem_init(&sem->sem2,0,1);
+    sem_init(&sem->sem1,0,1);//semaforo de la impresora 1
+    sem_init(&sem->sem2,0,1);//semaforo de la impresora 2
 
     int i;
     for(i=0;i<MAXUSERS;i++){
@@ -56,8 +59,8 @@ int main()
     for (i = 0; i < MAXUSERS; i++) {
 		pthread_join(threads[i], NULL);
     }
-    sem_destroy(&((semaforos *)sem)->sem1);
-    sem_destroy(&((semaforos *)sem)->sem2);
+    sem_destroy(&sem->sem1);
+    sem_destroy(&sem->sem2);
     free(sem);
     return 0;
 }
